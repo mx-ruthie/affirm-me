87d769d3f4f36f41617a7759799019535f4d77bb
Ruth Irvin <ruth.diane.irvin@gmail.com>
2022-11-18 12:35:52 -0600

    deletes more garbage notes

--- server/server.js
+++ server/server.js
@@ -48,54 +48,6 @@ app.get('/table', cors(), async (req, res) => {
   //TODO: in other circumstances maybe make all lowercase to compare exact strings but this will always hit the exact same strings so its not an issue.
 
 
-// create the POST request
-// app.post('/api/students', cors(), async (req, res) => {
-//   const newUser = {
-//     firstname: req.body.firstname,
-//     lastname: req.body.lastname,
-//   };
-//   console.log([newUser.firstname, newUser.lastname]);
-//   const result = await db.query(
-//     'INSERT INTO students(firstname, lastname) VALUES($1, $2) RETURNING *',
-//     [newUser.firstname, newUser.lastname],
-//   );
-//   console.log(result.rows[0]);
-//   res.json(result.rows[0]);
-// });
-
-//A put request - Update a student 
-// app.put('/api/students/:studentId', cors(), async (req, res) =>{
-//   console.log(req.params);
-//   //This will be the id that I want to find in the DB - the student to be updated
-//   const studentId = req.params.studentId
-//   const updatedStudent = { id: req.body.id, firstname: req.body.firstname, lastname: req.body.lastname}
-//   console.log("In the server from the url - the student id", studentId);
-//   console.log("In the server, from the react - the student to be edited", updatedStudent);
-//   // UPDATE students SET lastname = "something" WHERE id="16";
-//   const query = `UPDATE students SET lastname=$1, firstname=$2 WHERE id=${studentId} RETURNING *`;
-//   const values = [updatedStudent.lastname, updatedStudent.firstname];
-//   try {
-//     const updated = await db.query(query, values);
-//     console.log(updated.rows[0]);
-//     res.send(updated.rows[0]);
-
-//   }catch(e){
-//     console.log(e);
-//     return res.status(400).json({e})
-//   }
-// })
-
-// delete request
-// app.delete('/api/students/:studentId', cors(), async (req, res) =>{
-//   const studentId = req.params.studentId;
-//   //console.log("From the delete request-url", req.params);
-//   await db.query('DELETE FROM students WHERE id=$1', [studentId]);
-//   res.status(200).end();
-
-// });
-
-
-// removing example db query text here
 app.post('/api/me', cors(), async (req, res) => {
   const newUser = {
     lastname: req.body.family_name,
